c45dba5706cbc36cc6b41be50ca51ebc953397d7
Zeljko Toplak <zeljko.toplak@gmx.de>
2024-12-10 14:08:07 +0100

    Neue Klassen erstellt und registriert

--- Bestellung/admin.py
+++ Bestellung/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Lieferant, Lieferung
 
 # Register your models here.
+
+admin.site.register(Lieferant)
+admin.site.register(Lieferung)
--- Bestellung/models.py
+++ Bestellung/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Lieferant(models.Model):
+    name = models.CharField(("Name"), max_length=50)
+    email = models.CharField(("E-Mail"), max_length=50)
+    telefon = models.CharField(("Telefonnummer"), max_length=50)
+    
+
+    class Meta:
+        verbose_name = ("Lieferant")
+        verbose_name_plural = ("Lieferanten")
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse("Lieferant_detail", kwargs={"pk": self.pk})
+
+
+class Lieferung(models.Model):
+    datum = models.DateField(("Lieferdatum"), auto_now=False, auto_now_add=False)
+    lieferant = models.ForeignKey(Lieferant, verbose_name=("Lieferant"), on_delete=models.RESTRICT)
+
+    class Meta:
+        verbose_name = ("Lieferung")
+        verbose_name_plural = ("Lieferungen")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("Lieferung_detail", kwargs={"pk": self.pk})
